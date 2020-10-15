4a115ee7af80f4e63d21f3189448fe17298ad79b
R99bbit <rjsdid7325@gmail.com>
2020-10-15 15:22:32 +0900

    20201015 R99bbit IPLayer Init(implemented methods)

---
+++ src/chat_file/IPLayer.java
@@ -0,0 +1,47 @@
+package chat_file;
+
+/**
+ * @author parkmin(R99bbit@github)
+ * @interface BaseLayer(BaseLayer.java)
+ */
+
+import java.util.*;
+
+public class IPLayer implements BaseLayer {
+	@Override
+	public String GetLayerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseLayer GetUnderLayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseLayer GetUpperLayer(int nindex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void SetUnderLayer(BaseLayer pUnderLayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SetUpperLayer(BaseLayer pUpperLayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SetUpperUnderLayer(BaseLayer pUULayer) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
